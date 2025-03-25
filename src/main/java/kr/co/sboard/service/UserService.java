3b463ed680e4d7e30bca88003fa09eda1851237e
unknown <wkdalsgur5556@gmail.com>
2025-03-25 15:48:11 +0900

    sboadr2

--- src/main/java/kr/co/sboard/service/UserService.java
+++ src/main/java/kr/co/sboard/service/UserService.java
@@ -1,13 +1,25 @@
 package kr.co.sboard.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.NewsAddress;
+import jakarta.servlet.http.HttpSession;
 import kr.co.sboard.dto.UserDTO;
 import kr.co.sboard.entity.User;
 import kr.co.sboard.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -15,6 +27,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final JavaMailSender mailSender;
+
+    private final HttpSession session;
 
     public void register(UserDTO userDTO){
 
@@ -39,10 +54,55 @@ public class UserService {
             count = userRepository.countByNick(value);
         }else if(type.equals("email")){
             count = userRepository.countByEmail(value);
+
+            if(count == 0){
+                String code = sendEmailCode(value);
+
+                // 인증코드 비교를 하기 위해서 세션 저장
+                session.setAttribute("authCode", code);
+
+
+            }
+
+
+
+
+
         }else if(type.equals("hp")){
             count = userRepository.countByHp(value);
         }
         return count;
+    }
+
+    @Value("${spring.mail.username}")
+    private String sender;
+    public String sendEmailCode(String receiver){
+
+        // MimeMessage 생성
+        MimeMessage message = mailSender.createMimeMessage();
+
+        // 읹증코드 생성
+        int code = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        log.info("code: " + code);
+
+         String subject = "sboard 인증코드 안내";
+         String content = "<h1>sboard 인증코드는 " + code + " 입니다.</h1>";
+
+        try {
+
+            message.setFrom(new InternetAddress(sender, "보내는사람", "UTF-8"));
+            message.addRecipient(Message.RecipientType.TO,new  InternetAddress(receiver));
+            message.setSubject(subject);
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            mailSender.send(message);
+        }catch (Exception e){
+            log.error(e.getMessage());
+        }
+
+        return String.valueOf(code);
+
+
     }
 
 
